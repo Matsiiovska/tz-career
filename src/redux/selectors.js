d68f8d088577d147af0404bbff371219d4bd7b17
olia <dovgalyukolya@gmail.com>
2024-03-20 22:50:19 +0300

    nmmmm

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -7,7 +7,7 @@ export const selectorsCars = state => state.cars.cars;
 export const selectFilter = state => state.cars.filter;
 
 export const selectorsFavoriteCars = state => state.cars.favoriteCars;
-export const selectFavoriteCars = state => state.cars.favoriteCars;
+//export const selectFavoriteCars = state => state.cars.favoriteCars;
 
 export const selectVisibleCars = createSelector(
   [selectorsCars, selectFilter],
@@ -17,11 +17,13 @@ export const selectVisibleCars = createSelector(
     }
 
     return cars
-      .filter(one =>
-        brand !== '' && one.make !== undefined
+      .filter(one => {
+        console.log('one:', one);
+        console.log('one.make:', one.make);
+        return brand !== '' && one.make !== undefined
           ? one.make.toLowerCase().includes(brand.toLowerCase())
-          : one
-      )
+          : one;
+      })
       .filter(one => {
         const formattedPrice = Number(
           one.rentalPrice.slice(1, one.rentalPrice.length)
